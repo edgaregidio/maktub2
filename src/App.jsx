189a3aea40d3ab86cb9e2c24eb6cc58f87a6244f
Edgar Egidio <edgaregidio@MacBook-Air-de-Edgar.local>
2023-02-18 20:51:34 -0300

    scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
+import ScrollToTop from 'react-scroll-to-top'
 import Notice from './components/common/Notice'
 import Sections from './components/common/Sections'
 import Accordion from './components/core/Accordion'
 import Banner from './components/core/Banner'
-import CardDrinks from './components/core/CardDrinks'
 import Nav from './components/core/Nav'
 
 function App() {
 
   return (
-    <div className="App">
+    <>
+    <ScrollToTop
+      smooth
+      viewBox='0 0 256 256'
+      className='btnUp'
+      color='#222'
+      />
       <Nav />
       <Banner />
       <Notice click={'Clique para interagir!'}/>
       <Accordion />
       <Sections name={'DRINKS CLÁSSICOS'}/>
-    </div>
+    </>
   )
 }
 
