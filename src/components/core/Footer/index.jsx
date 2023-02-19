33a1f749066c3ff90e1501793c6a09deb4ae7d96
Edgar Egidio <edgaregidio@MacBook-Air-de-Edgar.local>
2023-02-19 17:09:42 -0300

    mudando meu instagram

--- src/components/core/Footer/index.jsx
+++ src/components/core/Footer/index.jsx
@@ -5,7 +5,7 @@ import './styles.css'
 function Footer() {
   return (
     <section class="linkDevsFooter">
-      <h5>Criado por <a href="https://www.instagram.com/edgardevv/" alt="EvoluTI" target="_blank">Edgar Egidio 💻 </a> </h5>
+      <h5>Criado por <a href="https://www.instagram.com/edgarsegidio/" alt="EvoluTI" target="_blank">Edgar Egidio 💻 </a> </h5>
     </section>
   )
 }
